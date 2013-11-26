cf49b238dfe5baf43fce1812fc92aab387c63331
Oleg Nenashev <nenashev@synopsys.com>
2013-11-27 00:11:18 +0400

    [FIXED JENKINS-20776] - Added validation of classNames in the form Resolves https://issues.jenkins-ci.org/browse/JENKINS-20776
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/org/jenkinsci/plugins/Exclusion.java
+++ src/main/java/org/jenkinsci/plugins/Exclusion.java
@@ -1,10 +1,13 @@
 package org.jenkinsci.plugins;
 
 import hudson.Extension;
+import hudson.ExtensionPoint;
 import hudson.Util;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
+import hudson.util.FormValidation;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 /**
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
@@ -34,6 +37,30 @@ public class Exclusion extends AbstractDescribableImpl<Exclusion> {
         @Override
         public String getDisplayName() {
             return "Extension";
+        }
+        
+        public FormValidation doCheckFqcn(@QueryParameter String fqcn) {
+            Class definedClass;
+            try {
+                definedClass = Class.forName(fqcn);
+            } catch (ClassNotFoundException e) {
+                return FormValidation.error(Messages.Exclusion_classNotFoundMsg());
+            }
+
+            final boolean isExtension = ExtensionPoint.class.isAssignableFrom(definedClass);
+            final boolean isDescriptor = Descriptor.class.isAssignableFrom(definedClass);
+
+            // Handle possible combinations
+            if (isExtension && isDescriptor) {
+                return FormValidation.ok(Messages.Exclusion_classIsExtensionAndDescriptor());
+            }
+            if (isExtension) {
+                return FormValidation.ok(Messages.Exclusion_classIsExtension());
+            }
+            if (isDescriptor) {
+                return FormValidation.ok(Messages.Exclusion_classIsDescriptor());
+            }
+            return FormValidation.error(Messages.Exclusion_unsupportedType());
         }
     }
 }
